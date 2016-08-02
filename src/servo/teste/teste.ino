152a4c1d9f0e11c7fa7b62004a45092746a7cdcd
Tiago Alves <tiago.alves718@gmail.com>
2016-08-02 15:10:01 -0300

    fix(servo): alterando servo test

--- src/servo/teste/teste.ino
+++ src/servo/teste/teste.ino
@@ -1,13 +1,9 @@
 #include <Servo.h>
 Servo servo;
-const int TrigPin = 10;
-const int EchoPin = 9;
 
 void setup() {  
-pinMode(TrigPin, OUTPUT);  
-pinMode(EchoPin, INPUT);    
-servo.attach(11);    
-Serial.begin(9600);}
+  servo.attach(11);
+}
 
 void loop() {
 servo.write(90);  
